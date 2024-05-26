19a5f6b135582c27403b4eaec6175fe2367d7c30
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2024-05-26 19:00:53 -0400

    removed unnecessary files

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css' // keeps tailwindcss styling for all pages
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
