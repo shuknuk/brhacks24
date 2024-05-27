45e212a5c484cde3d20f99f31e2762c1ba4f2a14
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2024-05-26 23:46:50 -0400

    navbar color update

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <header className="rounded-xl border-2 border-green-500 fixed top-4 left-10 right-10 z-50 backdrop-blur-sm shadow-md flex justify-between items-center h-24 px-5">
+    <header className="rounded-xl border-2 border-emerald-700 fixed top-4 left-10 right-10 z-50 backdrop-blur-sm shadow-md flex justify-between items-center h-24 px-5">
       <h1 className="text-4xl">Logo</h1>
       <nav className="flex gap-8">
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">About</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">FAQ</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">Schedule</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">Team</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">Sponsors</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">Previous Events</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-green-500">Register</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">About</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">FAQ</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Schedule</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Team</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Sponsors</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Previous Events</a>
+        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Register</a>
       </nav>
     </header>
   );
