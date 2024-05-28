a7ad5db3ea73053c2e0af7f504307307e696a48b
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2024-05-28 13:23:02 -0400

    Footer updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,26 +1,24 @@
-// Footer.jsx
-
 import React from 'react';
 
 const Footer = () => {
   return (
-    <footer id="footer" className="footercls">
-      <div className="footer_img_cls">
-        <a href="" target="_blank" rel="noopener noreferrer">
-          <img src="github.png" className="git_css" width="55" height="55" alt="github" />
+    <footer id="footer" className="relative bg-[#a4ccac] w-full text-white text-center font-serif leading-8 py-5">
+      <div className="flex justify-center items-center mb-4">
+        <a href="" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <img src="github.png" className="hover:scale-120 transition duration-350" width="55" height="55" alt="github" />
         </a>
         <span>&nbsp;</span>
-        <a href="https://www.instagram.com/brhs.hacks/" target="_blank" rel="noopener noreferrer">
-          <img src="insta.png" className="insta_css" width="50" height="50" alt="insta" />
+        <a href="https://www.instagram.com/brhs.hacks/" target="_blank" rel="noopener noreferrer" className="mx-2">
+          <img src="insta.png" className="hover:scale-120 transition duration-350" width="50" height="50" alt="insta" />
         </a>
       </div>
-      <div id="footer_copyrt">
+      <div id="footer_copyrt" className="text-base">
         Copyright © Hack BRHS
       </div>
-      <div className="footer_privacy">
-        <a href="https://mlh.io/privacy" target="_blank" rel="noopener noreferrer" className="footer_links">Privacy Policy</a>
+      <div className="text-lg">
+        <a href="https://mlh.io/privacy" target="_blank" rel="noopener noreferrer" className="text-white no-underline hover:font-bold mx-2">Privacy Policy</a>
         <span>|</span>
-        <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer" className="footer_links">Code of Conduct</a>
+        <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer" className="text-white no-underline hover:font-bold mx-2">Code of Conduct</a>
       </div>
     </footer>
   );
