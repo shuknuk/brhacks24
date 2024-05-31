94cae917edc3d8f26486b3343519c116315c2663
Kinshuk Goel <67846964+Kinshuk-Goel@users.noreply.github.com>
2024-05-31 12:41:28 -0400

    made new changes and adjustments

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,19 +1,31 @@
+// NavBar.jsx
+
 import React from 'react';
 import Link from 'next/link';
-
 
 const NavBar = () => {
   return (
     <header className="rounded-xl border-2 border-emerald-700 fixed top-4 left-10 right-10 z-50 backdrop-blur-sm shadow-md flex justify-between items-center h-24 px-5">
       <h1 className="text-4xl">Logo</h1>
       <nav className="flex gap-8">
-        <a href="/About" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">About</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">FAQ</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Schedule</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Team</a>
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Sponsors</a>
-        {/* <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Previous Events</a> */}
-        <a href="#" className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Register</a>
+        <Link href="/about">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">About</p>
+        </Link>
+        <Link href="/faq">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">FAQ</p>
+        </Link>
+        <Link href="/schedule">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Schedule</p>
+        </Link>
+        <Link href="/team">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Team</p>
+        </Link>
+        <Link href="/sponsors">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Sponsors</p>
+        </Link>
+        <Link href="/register">
+          <p className="text-lg font-semibold px-4 py-2 rounded-full transition duration-300 hover:bg-emerald-700">Register</p>
+        </Link>
       </nav>
     </header>
   );
